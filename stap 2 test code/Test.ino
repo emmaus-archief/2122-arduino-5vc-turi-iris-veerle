5cc7cc6533ce705a3be141660b74f7bef4b24db5
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2022-04-06 09:34:49 +0200

    test motor

---
+++ stap 2 test code/Test.ino
@@ -0,0 +1,28 @@
+
+#define PWMA   6           //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
+  
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT);  
+  analogWrite(PWMA,50);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  analogWrite(PWMB,50);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
