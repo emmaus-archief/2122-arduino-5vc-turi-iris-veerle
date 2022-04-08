64bc1cb199f794f56ccfc1b53b043f0fa1d92a3d
Toerieeeeeee <74536539+Toerieeeeeee@users.noreply.github.com>
2022-04-08 10:33:41 +0200

    Test code voor rijden en lijn zoeken

---
+++ stap 2 test code/line traking map/Infrared-Line-Tracking.ino
@@ -0,0 +1,104 @@
+#include "TRSensors.h"
+
+#define NUM_SENSORS 5
+TRSensors trs =   TRSensors();
+unsigned int sensorValues[NUM_SENSORS];
+unsigned int last_proportional = 0;
+long integral = 0;
+
+#define PWMA   6           //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("TRSensor example!!");
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT);  
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);  
+  
+  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  {
+    trs.calibrate();       // reads all sensors 10 times
+  }
+  Serial.println("calibrate done");
+  
+  // print the calibration minimum values measured when emitters were on
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(trs.calibratedMin[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  
+  // print the calibration maximum values measured when emitters were on
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    Serial.print(trs.calibratedMax[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Get the position of the line.  Note that we *must* provide
+  // the "sensors" argument to read_line() here, even though we
+  // are not interested in the individual sensor readings.
+  //trs.AnalogRead(sensorValues); 
+  unsigned int position = trs.readLine(sensorValues);
+//  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+//  {
+//    Serial.print(sensorValues[i]);
+//    Serial.print('\t');
+//  }
+//  Serial.println();
+  //Serial.println(position); // comment this line out if you are using raw values
+
+  // The "proportional" term should be 0 when we are on the line.
+  int proportional = (int)position - 2000;
+
+  // Compute the derivative (change) and integral (sum) of the position.
+  int derivative = proportional - last_proportional;
+  integral += proportional;
+
+  // Remember the last position.
+  last_proportional = proportional;
+  
+  // improve performance.
+  int power_difference = proportional/20 + integral/10000 + derivative*10;
+
+  // Compute the actual motor settings.  We never set either motor
+  // to a negative value.
+  const int maximum =150;
+
+  if (power_difference > maximum)
+    power_difference = maximum;
+  if (power_difference < - maximum)
+    power_difference = - maximum;
+   // Serial.println(power_difference);
+  if (power_difference < 0)
+  {
+    analogWrite(PWMA,maximum + power_difference);
+    analogWrite(PWMB,maximum);
+  }
+  else
+  {
+    analogWrite(PWMA,maximum);
+    analogWrite(PWMB,maximum - power_difference);
+  }    
+  //delay(250);
+}
